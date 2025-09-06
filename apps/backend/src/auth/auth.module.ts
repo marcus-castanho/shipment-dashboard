d832e3c65a0378f09c210c7fde2ceecb337ad501
marcus.castanho <castanhomarcus@gmail.com>
2025-09-06 17:03:21 -0300

    feat: remove unused route in auth controller

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { AuthGuard } from './auth.guard';
-import { APP_GUARD } from '@nestjs/core';
 import { jwtModule } from './dependencies/jwt.module';
 
 @Module({
   imports: [jwtModule],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
