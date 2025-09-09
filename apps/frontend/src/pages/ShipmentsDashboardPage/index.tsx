38a89128d4cce417fa7dc4d2bc68d2c581544c79
marcus.castanho <castanhomarcus@gmail.com>
2025-09-09 14:34:43 -0300

    feat: create shipments table

--- apps/frontend/src/pages/ShipmentsDashboardPage/index.tsx
+++ apps/frontend/src/pages/ShipmentsDashboardPage/index.tsx
@@ -1,4 +1,5 @@
 import { Main } from "../../components/Main";
+import { ShipmentsTable } from "./components/ShipmentsTable";
 import { UserSwitch } from "./components/UserSwitch";
 import { ShipmentWSProvider } from "./context/ShipmentWSContext";
 
@@ -8,6 +9,7 @@ export const ShipmentsDashboardPage = () => {
       <Main className="p-10 flex flex-col gap-4">
         <UserSwitch />
         <h1 className="text-4xl">Shipments Dashboard</h1>
+        <ShipmentsTable />
       </Main>
     </ShipmentWSProvider>
   );
