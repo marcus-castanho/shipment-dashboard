3af6f3885d50d1455863a7eea0a06192b839c721
marcus.castanho <castanhomarcus@gmail.com>
2025-09-09 13:53:06 -0300

    feat: update EVENT const codes and prisma ERROR const codes

--- apps/backend/src/prisma/consts.ts
+++ apps/backend/src/prisma/consts.ts
@@ -1,4 +1,5 @@
 export const PRISMA_ERROR = {
   P2002: { name: 'unique_constraint', code: 'P2002' },
+  P2025: { name: 'no_record_found_delete', code: 'P2025' },
   DEFAULT: { name: 'default', code: 'DEFAULT' },
 };
