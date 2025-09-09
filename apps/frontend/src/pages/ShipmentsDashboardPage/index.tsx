85093e9a2d8befa35033ed4374afcb3b095f5403
marcus.castanho <castanhomarcus@gmail.com>
2025-09-09 14:22:01 -0300

    feat: create UserSwitch

--- apps/frontend/src/pages/ShipmentsDashboardPage/index.tsx
+++ apps/frontend/src/pages/ShipmentsDashboardPage/index.tsx
@@ -1,11 +1,13 @@
 import { Main } from "../../components/Main";
+import { UserSwitch } from "./components/UserSwitch";
 import { ShipmentWSProvider } from "./context/ShipmentWSContext";
 
 export const ShipmentsDashboardPage = () => {
   return (
     <ShipmentWSProvider>
       <Main className="p-10 flex flex-col gap-4">
-        <></>
+        <UserSwitch />
+        <h1 className="text-4xl">Shipments Dashboard</h1>
       </Main>
     </ShipmentWSProvider>
   );
